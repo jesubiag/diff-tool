31a73532846b5a60005ab808ebb78b2a7ab93387
Jesus Biaggioni <jesu772@gmail.com>
2023-09-09 14:44:54 -0300

    Comparison of two null objects + added hamcrest dependency

--- src/main/kotlin/com/difftool/DiffTool.kt
+++ src/main/kotlin/com/difftool/DiffTool.kt
@@ -2,8 +2,11 @@ package com.difftool
 
 object DiffTool {
 
-    fun <T> diff(o1: T, o2: T): List<ChangeType> {
-        TODO()
+    fun <T> diff(o1: T, o2: T): List<ChangeType> = when {
+        bothObjectsAreNull(o1, o2) -> emptyList()
+        else -> TODO()
     }
+
+    private fun <T> bothObjectsAreNull(o1: T, o2: T): Boolean = o1 == null && o2 == null
 
 }
--- src/test/kotlin/com/difftool/DiffToolTest.kt
+++ src/test/kotlin/com/difftool/DiffToolTest.kt
@@ -1,13 +1,19 @@
 package com.difftool
 
-import org.junit.jupiter.api.Assertions
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 
 class DiffToolTest {
 
     @Test
-    fun test() {
-        Assertions.assertTrue(true)
+    fun `should return empty for two null objects`() {
+        val o1 = null
+        val o2 = null
+
+        val diff = DiffTool.diff(o1, o2)
+
+        assertThat(diff, empty())
     }
 
 }
